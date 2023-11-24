d1432867097bf0e44129e4c88f6c0d3e6ec2f3da
Annapoorani-71 <annapooranikumar71@gmail.com>
2023-11-24 12:46:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import { useState } from "react";
+//client->src->app.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
 function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const handleOnSubmit = async (e) => {
-    e.preventDefault();
-    let result = await fetch("http://localhost:5000/register", {
-      method: "post",
-      body: JSON.stringify({ name, email }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    result = await result.json();
-    console.warn(result);
-    if (result) {
-      alert("Data saved succesfully");
-      setEmail("");
-      setName("");
-    }
-  };
+  const [userNames, setUserNames] = useState([]);
+
+  const handlePost = () => {
+    axios.post('http://localhost:5000/register', { name, email })//ameen, ameen@gmail.com
+      .then((response) => {
+        const result = response.data;
+        if (result) {
+          alert("Data saved successfully");
+          setEmail("");
+          setName("");
+        }
+      })
+      .catch((error) => {
+        console.error("POST request error:", error);
+        alert("Something went wrong when saving data.");
+      });
+  }
+
+  const handleGet = () => {
+    axios.get('http://localhost:5000/')
+      .then((response) => {               //thejaswini, jaya, ameen
+        const users = response.data;      //names -> array
+        const names = users.map((user) => user.name);   //map-> used for iteration
+        setUserNames(names);
+      })
+      .catch((error) => {
+        console.error("GET request error:", error);
+      });
+  }
+
   return (
-    <>
-      <h1>This is React WebApp </h1>
-      <form action="">
+    <div className="container">
+      <h1>From</h1>
+      <form>
         <input
           type="text"
           placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={name}        //take the value from the user
+          onChange={(f) => setName(f.target.value)}
         />
         <input
           type="email"
@@ -35,13 +52,12 @@ function App() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
- 
-        <button type="submit" onClick={handleOnSubmit}>
-          submit
-        </button>
+        <button type="button" onClick={handlePost}>Post</button>
+        <button type="button" onClick={handleGet}>Get</button>
       </form>
-    </>
+      <p>Names: {userNames.join(", ")}</p>
+    </div>
   );
 }
- 
-export default App; 
+
+export default App;
